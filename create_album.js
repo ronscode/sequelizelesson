236e3cd72a227b686f0a4baeb46337d25121f2f7
Ron Cagle <ron@atlantashirt.com>
2019-01-04 18:46:10 -0500

    The create album is working

--- create_album.js
+++ create_album.js
@@ -22,18 +22,32 @@ app.set('views', 'app/views');
 // });
 
 const newAlbumName = "";
+const newAlbumYear = "";
+
 rl.question('What is the album name? ', (NewAlbumName) => {
+    rl.question('Year the album was released? ', (newAlbumYear) => {
+
     // TODO: Log the answer in a database
-
-
     console.log(`Thank you for your valuable feedback: ${NewAlbumName}`);
-
-    models.album2.create({albumName:`${NewAlbumName}`})
+    
+    models.album2.create({albumName:`${NewAlbumName}`, albumYear:`${newAlbumYear}`})
   .then(function(album2){
         console.log(album2);
   });
+
     rl.close();
+});
   });
+
+//   rl.question('Please enter the first number : ', (answer1) => {
+//     rl.question('Please enter the second number : ', (answer2) => {
+//         var result = (+answer1) + (+answer2);
+//         console.log(`The sum of above two numbers is ${result}`);
+//         rl.close();
+//     });
+// });
+  
+
 
 // This creates a user in the DB 
 
